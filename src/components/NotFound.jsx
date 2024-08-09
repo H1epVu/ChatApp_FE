057bc86c5b8edb3ba046c60267da7c14d37d5d6d
H1ep Vu <hiepvu.tech02@gmail.com>
2024-08-09 22:34:46 +0700

    add 404 page

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,11 @@
+const NotFound = () => {
+    return (
+        <>
+            <div className='mt-5 d-flex justify-content-center'>
+                <h1 className='mt-5'>Không tìm thấy trang</h1>
+            </div>
+        </>
+    );
+}
+
+export default NotFound;
