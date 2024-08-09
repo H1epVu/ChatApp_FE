3e68fdc3135ef9a7c0a8154f0715dd8e5ea12cf4
H1ep Vu <hiepvu.tech02@gmail.com>
2024-08-09 22:35:59 +0700

    config file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,40 @@
-import { useState } from 'react'
+import { Routes, Route, Navigate } from "react-router-dom"
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Contact from "./pages/Contact";
+import NotFound from "./components/NotFound";
+import "bootstrap/dist/css/bootstrap.min.css"
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import { Container } from "react-bootstrap";
+import NavBar from "./components/NavBar";
+import "./style.scss";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    </>
+    <div className="app">
+      <NavBar />
+      <Container className="text-secondary">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Container>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={true}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+      />
+    </div>
   )
 }
 
